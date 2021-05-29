4950d4e70bb66fc84eec6ea81ce2d18e9f223e7a
Francis <francisnvinner@gmail.com>
2021-05-29 21:37:15 +0300

    Setup the react redux store connected with the router.

--- app/frontend/packs/Index.jsx
+++ app/frontend/packs/Index.jsx
@@ -1,10 +1,33 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import thunkMiddleware from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { ConnectedRouter,routerMiddleware } from 'connected-react-router';
 
-import App from '../components/App'
+import rootReducer from '../redux/reducers/index_reducer';
+import App from '../components/App';
+
+const history = createBrowserHistory();
+
+export const store = createStore(
+    rootReducer(history),
+    composeWithDevTools(
+        applyMiddleware(
+            thunkMiddleware,
+            routerMiddleware(history)
+        )
+    )
+)
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<App/>, document.body.appendChild(document.createElement('div')));
+    ReactDOM.render(
+        <Provider store={store}>
+            <ConnectedRouter history={history} hasTimeTravel={false}>
+                <App/>
+            </ConnectedRouter>
+        </Provider>, 
+        document.body.appendChild(document.createElement('div')));
 });
