db7ac01f63a880b7b1408be0d9d82df00ffdbde4
Francis <francisnvinner@gmail.com>
2021-05-29 22:49:02 +0300

    Setup the endpoints for the devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users, defaults: { format: :json },
+                     path_names: {
+                       sign_in: 'login',
+                       sign_out: 'logout'
+                     },
+                     controllers: {
+                       registrations: 'api/v1/registrations',
+                       sessions: 'api/v1/sessions'
+                     }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'homepage#index'
