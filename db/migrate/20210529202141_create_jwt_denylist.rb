7d9356b472e85ac89958f78770b42d8b30e272a4
Francis <francisnvinner@gmail.com>
2021-05-30 09:57:54 +0300

    Create the jwt_denylist table.

---
+++ db/migrate/20210529202141_create_jwt_denylist.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Migration for create the JWTDenylist table
+class CreateJwtDenylist < ActiveRecord::Migration[6.0]
+  def change
+    create_table :jwt_denylist do |t|
+      t.string :jti, null: false
+      t.datetime :exp, null: false
+    end
+    add_index :jwt_denylist, :jti
+  end
+end
