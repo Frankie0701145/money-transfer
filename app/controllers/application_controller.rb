2df9f1e4d049de5b3432eee68310cf4d511bfef5
Francis <francisnvinner@gmail.com>
2021-05-30 14:51:14 +0300

    Skip the verify_authenticity_token in if the request is of json format.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
+# The main controller.
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token, if: :json_request?
+
+  def json_request?
+    request.format.json?
+  end
 end
