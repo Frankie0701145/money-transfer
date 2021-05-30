a5922b97626af90873646fd5e726f8d19b02075c
Francis <francisnvinner@gmail.com>
2021-05-30 16:33:18 +0300

    Setup the endpoint for creating sessions.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,20 @@
-class Api::V1::SessionsController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  module V1
+    # SessionsController for login and logout
+    class SessionsController < Devise::SessionsController
+      respond_to :json
+
+      private
+
+      def respond_with(resource, _opt = {})
+        render json: UserSerializer.new(resource).serialized_json, status: 200
+      end
+
+      def respond_to_on_destroy
+        head :ok
+      end
+    end
+  end
 end
