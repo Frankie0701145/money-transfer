6013672f673c736e456f2a0ecba25ce9ac7d2262
Francis <francisnvinner@gmail.com>
2021-05-30 12:42:55 +0300

    Generate the user_serializer and created the registrations_controller.

--- app/controllers/api/vi/registrations_controller.rb
+++ app/controllers/api/vi/registrations_controller.rb
@@ -1,2 +1,29 @@
-class Api::Vi::RegistrationsController < ApplicationController
+# frozen_string_literal: true
+
+module Api
+  module ModuleName
+    # The Registration Controller
+    class RegistrationsController < Devise::RegistrationsController
+      respond_to :json
+      before_action :build_userable, only: [:create]
+
+      def create
+        build_resource(registration_params)
+        if resource.save
+          render json: UserSerializer.new(resource).serialized_json,
+                 status: :created
+        else
+          render json: { type: 'error', errors: resource.errors.full_messages },
+                 status: 422
+        end
+      end
+
+      private
+
+      def registration_params
+        params.require(:registration)
+              .permit(%i[password email phone_number first_name last_name])
+      end
+    end
+  end
 end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  last_name              :string
+#  phone_number           :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+class UserSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :phone_number, :email,
+             :created_at, :updated_at
+  attribute full_name do |object|
+    "#{object.first_name.capitalize} #{object.last_name.capitalize}"
+  end
+end
