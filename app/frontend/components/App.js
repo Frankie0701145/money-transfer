f3bbbc03db24ae934397d90b2eb9e7e275e65ea2
Francis <francisnvinner@gmail.com>
2021-05-31 04:36:07 +0300

    Created the Homepage.

--- app/frontend/components/App.js
+++ app/frontend/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import HomePage from "./HomePage/HomePage";
+import {Switch, Route} from 'react-router-dom';
+import "./App.css";
+import 'antd/dist/antd.css';
 
 const App = ()=>{
     return(
-        <h1>Hello</h1>
+        <Switch>
+            <Route exact path="/" component={HomePage}/>
+        </Switch>
     )
 }
 
---
+++ app/frontend/components/HomePage/HomePage.js
@@ -0,0 +1,119 @@
+import React, {Component} from "react";
+import {
+    Col, Row, Typography, Layout, Button, Space
+} from "antd";
+import logo from "../../images/money_transfer.png";
+import {ArrowRightOutlined} from "@ant-design/icons";
+
+import "./HomePage.css";
+
+
+
+const {Title, Paragraph} = Typography;
+const { Header, Content, Footer, Sider } = Layout;
+
+/**
+ * This is the HomePage
+ * @component
+ * @example
+ * return(
+ *      <HomePage/>
+ * )
+ */
+class HomePage extends Component {
+    render(){
+        return(
+            <Layout style={{height: "100vh"}}>
+                
+                <Row justify="center" id="header" align="middle">
+                    <Col xs={{span: 23}}>
+                        <Row justify="space-between" align="middle">
+                            <Col xs={{span: 3}}>
+                                <span>
+                                    <img src={logo} id="homepage-logo"/>
+                                </span>
+                            </Col>
+
+                            <Col>
+                                <Row align="middle">
+                                    <Col>
+                                        <Button type="link">
+                                            <Title level={5} style={{color: "whitesmoke"}}>
+                                                Sign in
+                                            </Title>
+                                        </Button>
+                                    </Col>
+                                    <Col>
+                                        <Button type="primary" 
+                                            style={{
+                                                backgroundColor: "green",
+                                                borderColor: "green",
+                                                borderRadius: "0.2em"
+                                            }}
+                                        
+                                        >
+                                            <Title level={5} style={{color: "white"}}>
+                                                Get Started
+                                            </Title>
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        
+                    </Col>
+                </Row>
+                
+                <Row justify="center" style={{marginTop: "6em"}} id="content" >
+
+                    <Col xs={{span: 20}}>
+
+                        <Row align="middle">
+                            
+                            <Col xs={{span: 24}} md={{span:12}}>
+                                <Button type="link">
+                                    <Space>
+                                        <span> Get Started </span>
+                                        <ArrowRightOutlined />
+                                    </Space>
+                                    
+                                </Button>
+                                <Paragraph style={{fontSize: "2em"}}>
+                                    Get Started <br/> with the Best <br/> reliable money transfer
+                                </Paragraph>
+                                <Button type="primary" 
+                                    style={{
+                                        backgroundColor: "green",
+                                        borderColor: "green",
+                                        borderRadius: "0.2em"
+                                    }}
+                                
+                                >
+                                    <Title level={5} style={{color: "white"}}>
+                                        Get Started
+                                    </Title>
+                                </Button>
+
+                                <Button type="link">
+                                    <Title level={5} style={{color: "green"}}>
+                                        Sign in
+                                    </Title>
+                                </Button>
+
+                            </Col>
+
+                            <Col xs={{span: 0}} md={{span:12}} id="money-transfer-illustrator">
+                            
+                            </Col>
+
+                        </Row>
+                    </Col>
+
+                </Row>
+
+            </Layout>
+        )
+    }
+}
+
+export default HomePage;
