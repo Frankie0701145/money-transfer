4a45457cd536e8457beb539e6c6ff2e6e74ccd3e
Francis <francisnvinner@gmail.com>
2021-06-13 01:38:01 +0300

    Creates a static method in the MTransaction model for fetching all m_transaction from today.

--- app/models/m_transaction.rb
+++ app/models/m_transaction.rb
@@ -20,4 +20,7 @@
 class MTransaction < ApplicationRecord
   belongs_to :account
   belongs_to :transactable, polymorphic: true
+  def self.today
+    where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+  end
 end
